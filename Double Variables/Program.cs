bac70d51c664a18d57863eb90f4eba7f7097c1c1
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 10:59:59 +0300

    Proje dosyası ekle.

---
+++ Double Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double sayi;
+            //sayi = 25.9;
+            //Console.WriteLine(sayi);
+
+            double s1, s2, ort;
+            s1 = 91;
+            s2 = 82;
+            ort = (s1 + s2) / 2;
+            Console.WriteLine(ort);
+
+            Console.Read();
+        }
+    }
+}
